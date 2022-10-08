970ee7d70003c3a17a6fe25da69c18b9b543595f
some-one-mmx <maominxian1@huawei.com>
2022-10-08 19:00:12 +0800

    add pre dns interfaces
    Change-Id: I9430c0ba286b910f7d6e649e4f9864a83bded70a
    Signed-off-by: some-one-mmx <maominxian1@huawei.com>

---
+++ ohos_adapter/distributeddatamgr_adapter/webdatabase/src/ohos_web_dns_data_base_adapter_impl.cpp
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ohos_web_dns_data_base_adapter_impl.h"
+
+#include <cinttypes>
+#include "foundation/ability/ability_runtime/interfaces/kits/native/appkit/ability_runtime/context/application_context.h"
+#include "nweb_log.h"
+#include "sqlite_database_utils.h"
+
+using namespace OHOS::NativeRdb;
+using namespace OHOS::NWeb;
+
+namespace {
+    const int32_t RDB_VERSION = 1;
+    const std::string WEB_DNS_DATABASE_FILE = "web_dns.db";
+
+    const std::string ID_COL = "_id";
+    const std::string DNS_TABLE_NAME = "dns";
+    const std::string HOSTNAME_COL = "hostname";
+
+    const std::string CREATE_TABLE = "CREATE TABLE " + DNS_TABLE_NAME + " (" + ID_COL + " INTEGER PRIMARY KEY, " +
+                                     HOSTNAME_COL + " TEXT, " + " UNIQUE (" + HOSTNAME_COL + ") ON CONFLICT REPLACE);";
+                                     
+    #define MAX_HOSTNAME_NUM 20
+}
+
+int32_t DnsDataBaseRdbOpenCallBack::OnCreate(OHOS::NativeRdb::RdbStore& rdbStore)
+{
+    WVLOG_I("web dns database opened, create table: %{public}s", CREATE_TABLE.c_str());
+    return rdbStore.ExecuteSql(CREATE_TABLE);
+}
+
+int32_t DnsDataBaseRdbOpenCallBack::OnUpgrade(OHOS::NativeRdb::RdbStore& rdbStore,
+                                              int32_t currentVersion, int32_t targetVersion)
+{
+    WVLOG_I("web dns database upgrade");
+    return OHOS::NativeRdb::E_OK;
+}
+
+OhosWebDnsDataBaseAdapterImpl& OhosWebDnsDataBaseAdapterImpl::GetInstance()
+{
+    WVLOG_I("webdatabase get dns data base instance");
+    static OhosWebDnsDataBaseAdapterImpl instance;
+    if (instance.rdbStore_ == nullptr) {
+        WVLOG_I("webdatabase CreateDataBase");
+        DnsDataBaseRdbOpenCallBack callBack;
+        instance.rdbStore_ = instance.CreateDataBase(WEB_DNS_DATABASE_FILE, callBack);
+    }
+    return instance;
+}
+
+std::shared_ptr<OHOS::NativeRdb::RdbStore> OhosWebDnsDataBaseAdapterImpl::CreateDataBase(
+    const std::string& dataBeseName, OHOS::NativeRdb::RdbOpenCallback& callBack)
+{
+    WVLOG_I("web dns database create rdb store");
+    std::shared_ptr<OHOS::NativeRdb::RdbStore> rdbStore = nullptr;
+    std::shared_ptr<AbilityRuntime::ApplicationContext> context =
+        OHOS::AbilityRuntime::ApplicationContext::GetApplicationContext();
+    if (context == nullptr) {
+        WVLOG_E("web dns database get context failed");
+        return rdbStore;
+    }
+    std::string bundleName = context->GetBundleName();
+    std::string databaseDir = context->GetDatabaseDir();
+    std::string name = dataBeseName;
+    int32_t errorCode = E_OK;
+    std::string realPath = SqliteDatabaseUtils::GetDefaultDatabasePath(databaseDir, name, errorCode);
+    RdbStoreConfig config("");
+    config.SetPath(std::move(realPath));
+    config.SetBundleName(bundleName);
+    config.SetName(std::move(name));
+    config.SetArea(context->GetArea());
+    WVLOG_I("web dns database databaseDir=%{public}s", databaseDir.c_str());
+    WVLOG_I("web dns database bundleName=%{public}s", bundleName.c_str());
+
+    int32_t errCode = NativeRdb::E_OK;
+    rdbStore = RdbHelper::GetRdbStore(config, RDB_VERSION, callBack, errCode);
+    WVLOG_I("web dns database create rdb store end, errCode=%{public}d", errCode);
+    return rdbStore;
+}
+
+OhosWebDnsDataBaseAdapterImpl::OhosWebDnsDataBaseAdapterImpl() {}
+
+bool OhosWebDnsDataBaseAdapterImpl::ExistHostname(const std::string& hostname) const
+{
+    WVLOG_I("web dns database check exist hostname=%{public}s info", hostname.c_str());
+    if (rdbStore_ == nullptr || hostname.empty()) {
+        return false;
+    }
+
+    std::vector<std::string> hostInfo;
+    GetHostnames(hostInfo);
+    if (hostInfo.empty()) {
+        WVLOG_I("web dns database get no hostinfo.");
+        return false;
+    }
+
+    std::vector<std::string>::iterator it = find(hostInfo.begin(), hostInfo.end(), hostname);
+    if (it != hostInfo.end()) {
+        WVLOG_I("web dns database hostname does not exist.");
+        return true;
+    }
+    return false;
+}
+
+void OhosWebDnsDataBaseAdapterImpl::InsertHostname(const std::string& hostname)
+{
+    WVLOG_I("web dns database set info hostname:%{public}s", hostname.c_str());
+    if (rdbStore_ == nullptr || hostname.empty()) {
+        return;
+    }
+
+    if (CountHostname() >= MAX_HOSTNAME_NUM) {
+        DeleteOldestHostname();
+    }
+
+    int32_t errCode;
+    int64_t outRowId;
+    NativeRdb::ValuesBucket valuesBucket;
+    valuesBucket.Clear();
+    valuesBucket.PutString(HOSTNAME_COL, hostname);
+    errCode = rdbStore_->Insert(outRowId, DNS_TABLE_NAME, valuesBucket);
+    WVLOG_I("web dns database set info end, errCode=%{public}d", errCode);
+}
+
+void OhosWebDnsDataBaseAdapterImpl::GetHostnames(std::vector<std::string>& hostnames) const
+{
+    if (rdbStore_ == nullptr) {
+        return;
+    }
+
+    std::vector<std::string> columns;
+    NativeRdb::AbsRdbPredicates dirAbsPred(DNS_TABLE_NAME);
+    std::unique_ptr<AbsSharedResultSet> resultSet = rdbStore_->Query(dirAbsPred, columns);
+    if ((resultSet == nullptr) || (resultSet->GoToFirstRow() != NativeRdb::E_OK)) {
+        WVLOG_E("web dns database rdb store query failed");
+        return;
+    }
+    int32_t columnIndex;
+    resultSet->GetColumnIndex(HOSTNAME_COL, columnIndex);
+    do {
+        std::string hostname;
+        resultSet->GetString(columnIndex, hostname);
+        hostnames.push_back(hostname);
+    } while (resultSet->GoToNextRow() == NativeRdb::E_OK);
+}
+
+void OhosWebDnsDataBaseAdapterImpl::DeleteOldestHostname()
+{
+    WVLOG_I("web dns database clear oldest hostname");
+    if (rdbStore_ == nullptr) {
+        return;
+    }
+
+    std::vector<std::string> hostInfo;
+    GetHostnames(hostInfo);
+    if (hostInfo.empty()) {
+        WVLOG_I("web dns database no hostinfo.");
+        return;
+    }
+    // delete the oldest cached hostname:hostInfo[0]
+    int32_t deletedRows = 0;
+    NativeRdb::AbsRdbPredicates dirAbsPred(DNS_TABLE_NAME);
+    dirAbsPred.EqualTo(HOSTNAME_COL, hostInfo[0]);
+    int32_t ret = rdbStore_->Delete(deletedRows, dirAbsPred);
+    WVLOG_I("web dns database clear info: hostname=%{public}s, ret=%{public}d, deletedRows=%{public}d",
+            hostInfo[1].c_str(), ret, deletedRows);
+}
+
+int64_t OhosWebDnsDataBaseAdapterImpl::CountHostname() const
+{
+    WVLOG_I("web dns database count hostname info");
+    int64_t outValue = 0;
+    if (rdbStore_ == nullptr) {
+        return outValue;
+    }
+
+    NativeRdb::AbsRdbPredicates dirAbsPred(DNS_TABLE_NAME);
+    if (rdbStore_->Count(outValue, dirAbsPred) != NativeRdb::E_OK) {
+        return outValue;
+    }
+    WVLOG_I("web dns database count hostname info num = %{public}lld", outValue);
+    return outValue;
+}
