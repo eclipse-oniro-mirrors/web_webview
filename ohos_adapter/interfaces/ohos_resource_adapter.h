c627bdd7fa85f56270ad78d511eebac3d0cb4477
xiongjun_gitee <xiongjun28@huawei.com>
2022-11-14 15:56:06 +0800

    hap package is not decompressed
    Signed-off-by: xiongjun_gitee <xiongjun28@huawei.com>

---
+++ ohos_adapter/interfaces/ohos_resource_adapter.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_RESOURCE_ADAPTER_H
+#define OHOS_RESOURCE_ADAPTER_H
+
+#include <string>
+#include <sys/types.h>
+
+namespace OHOS::NWeb {
+class OhosResourceAdapter {
+public:
+    OhosResourceAdapter() = default;
+    virtual ~OhosResourceAdapter() = default;
+
+    virtual bool GetRawFileData(const std::string& rawFile, size_t& len,
+        std::unique_ptr<uint8_t[]>& dest, bool isSys = false) = 0;
+};
+}  // namespace OHOS::NWeb
+
+#endif  // OHOS_RESOURCE_ADAPTER_H
