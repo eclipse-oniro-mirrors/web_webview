44d33df1741f852469e5f7c4c1ef10f6782a4a23
xiongjun_gitee <xiongjun28@huawei.com>
2022-12-16 18:03:01 +0800

    hap包不解压适配
    Signed-off-by: xiongjun_gitee <xiongjun28@huawei.com>

--- ohos_adapter/ohos_resource_adapter/include/ohos_resource_adapter_impl.h
+++ ohos_adapter/ohos_resource_adapter/include/ohos_resource_adapter_impl.h
@@ -16,10 +16,38 @@
 #ifndef OHOS_RESOURCE_ADAPTER_IMPL_H
 #define OHOS_RESOURCE_ADAPTER_IMPL_H
 
-#include "ohos_resource_adapter.h"
 #include "extractor.h"
+#include "file_mapper.h"
+#include "ohos_resource_adapter.h"
 
 namespace OHOS::NWeb {
+class OhosFileMapperImpl : public OhosFileMapper {
+public:
+    OhosFileMapperImpl(std::unique_ptr<OHOS::AbilityBase::FileMapper> fileMap,
+        const std::shared_ptr<OHOS::AbilityBase::Extractor>& extractor);
+
+    virtual ~OhosFileMapperImpl() = default;
+
+    int32_t GetFd() const override;
+
+    int32_t GetOffset() const override;
+
+    std::string GetFileName() const override;
+
+    bool IsCompressed() const override;
+
+    void* GetDataPtr() const override;
+
+    size_t GetDataLen() const override;
+
+    bool UnzipData(std::unique_ptr<uint8_t[]>& dest, size_t& len) override;
+
+private:
+    std::shared_ptr<OHOS::AbilityBase::Extractor> extractor_ = nullptr;
+
+    std::unique_ptr<OHOS::AbilityBase::FileMapper> fileMap_ = nullptr;
+};
+
 class OhosResourceAdapterImpl : public OhosResourceAdapter {
 public:
     OhosResourceAdapterImpl(const std::string& hapPath);
@@ -29,11 +57,30 @@ public:
     bool GetRawFileData(const std::string& rawFile, size_t& len,
         std::unique_ptr<uint8_t[]>& dest, bool isSys = false) override;
 
+    bool GetRawFileMapper(const std::string& rawFile, std::unique_ptr<OhosFileMapper>& dest,
+        bool isSys = false) override;
+
+    bool IsRawFileExist(const std::string& rawFile, bool isSys = false) override;
+
+    bool GetRawFileLastModTime(const std::string& rawFile,
+        uint16_t& date, uint16_t& time, bool isSys = false) override;
+
+    bool GetRawFileLastModTime(const std::string& rawFile, time_t& time, bool isSys = false) override;
+
 private:
     void Init(const std::string& hapPath);
     
     static bool GetRawFileData(const std::shared_ptr<OHOS::AbilityBase::Extractor>& manager,
         const std::string& rawFile, size_t& len, std::unique_ptr<uint8_t[]>& dest);
+
+    static bool HasEntry(const std::shared_ptr<OHOS::AbilityBase::Extractor>& manager,
+        const std::string& rawFile);
+
+    static bool GetFileInfo(const std::shared_ptr<OHOS::AbilityBase::Extractor>& manager,
+        const std::string& rawFile, OHOS::AbilityBase::FileInfo& info);
+
+    static bool GetRawFileMapper(const std::shared_ptr<OHOS::AbilityBase::Extractor>& manager,
+        const std::string& rawFile, std::unique_ptr<OhosFileMapper>& dest);
 
     std::shared_ptr<OHOS::AbilityBase::Extractor> sysExtractor_;
     std::shared_ptr<OHOS::AbilityBase::Extractor> extractor_;
