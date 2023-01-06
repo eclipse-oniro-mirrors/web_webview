07511d8c0699a2fb4fcaecdfce368748e61c6856
zhouliting <zhouliting5@huawei.com>
2023-01-06 09:52:34 +0800

    Add TDD test case
    Signed-off-by: zhouliting <zhouliting5@huawei.com>

--- test/unittest/ohos_adapter/ohos_resource_adapter_test/ohos_resource_adapter_test.cpp
+++ test/unittest/ohos_adapter/ohos_resource_adapter_test/ohos_resource_adapter_test.cpp
@@ -85,16 +85,75 @@ HWTEST_F(OhosResourceAdapterTest, OhosResourceAdapterTest_GetRawFileData_002, Te
     std::string hapPath = "/system/app/com.ohos.nweb/NWeb.hap";
     OhosResourceAdapterImpl adapterImpl(hapPath);
     std::unique_ptr<uint8_t[]> dest;
-    size_t len = hapPath.size();
-    bool result = adapterImpl.GetRawFileData(hapPath, len, dest, true);
+    std::string rawFile = "test_web";
+    size_t len = rawFile.size();
+    bool result = adapterImpl.GetRawFileData(rawFile, len, dest, true);
     EXPECT_FALSE(result);
-    std::shared_ptr<Extractor> extractor = std::make_shared<Extractor>(hapPath);
-    extractor->Init();
-    extractor->initial_ = true;
-    result = adapterImpl.GetRawFileData(extractor, hapPath, len, dest);
+    std::shared_ptr<Extractor> extractor = std::make_shared<Extractor>(rawFile);
+    result = adapterImpl.GetRawFileData(extractor, rawFile, len, dest);
     EXPECT_FALSE(result);
-    result = adapterImpl.GetRawFileData(nullptr, hapPath, len, dest);
+    result = adapterImpl.GetRawFileData(nullptr, rawFile, len, dest);
     EXPECT_FALSE(result);
+    std::unique_ptr<OhosFileMapper> fileMapper = nullptr;
+    result = adapterImpl.GetRawFileMapper(rawFile, fileMapper, true);
+    EXPECT_FALSE(result);
+    result = adapterImpl.IsRawFileExist(rawFile, true);
+    EXPECT_FALSE(result);
+    uint16_t date;
+    uint16_t time;
+    result = adapterImpl.GetRawFileLastModTime(rawFile, date, time, true);
+    EXPECT_FALSE(result);
+    time_t times;
+    result = adapterImpl.GetRawFileLastModTime(rawFile, times, true);
+    EXPECT_FALSE(result);
+    result = adapterImpl.HasEntry(extractor, rawFile);
+    EXPECT_FALSE(result);
+    result = adapterImpl.HasEntry(nullptr, rawFile);
+    EXPECT_FALSE(result);
+    std::unique_ptr<OhosFileMapper> mapper = nullptr;
+    result = adapterImpl.GetRawFileMapper(nullptr, rawFile, mapper);
+    EXPECT_FALSE(result);
+    result = adapterImpl.GetRawFileMapper(extractor, rawFile, mapper);
+    EXPECT_FALSE(result);
+    adapterImpl.sysExtractor_.reset();
+    result = adapterImpl.GetRawFileLastModTime(rawFile, date, time, true);
+    EXPECT_FALSE(result);
+    result = adapterImpl.GetRawFileLastModTime(rawFile, times, true);
+    EXPECT_FALSE(result);
+}
+
+/**
+ * @tc.name: OhosResourceAdapterTest_OhosFileMapperImpl_003
+ * @tc.desc: OhosFileMapperImpl.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(OhosResourceAdapterTest, OhosResourceAdapterTest_OhosFileMapperImpl_003, TestSize.Level1)
+{
+    std::string rawFile = "test_web";
+    size_t len = rawFile.size();
+    std::shared_ptr<Extractor> extractor = std::make_shared<Extractor>(rawFile);
+    std::unique_ptr<OHOS::AbilityBase::FileMapper> fileMap = std::make_unique<OHOS::AbilityBase::FileMapper>();
+    fileMap->CreateFileMapper(rawFile, true, 0, 1, len);
+    OhosFileMapperImpl apperImpl(std::move(fileMap), extractor);
+    int32_t result = apperImpl.GetFd();
+    EXPECT_EQ(result, -1);
+    result = apperImpl.GetOffset();
+    EXPECT_NE(result, -1);
+    std::string fileNmae = apperImpl.GetFileName();
+    EXPECT_NE(fileNmae, "");
+    bool isCompressed = apperImpl.IsCompressed();
+    EXPECT_TRUE(isCompressed);
+    void* data = apperImpl.GetDataPtr();
+    EXPECT_EQ(data, nullptr);
+    size_t dataLen = apperImpl.GetDataLen();
+    EXPECT_NE(dataLen, 0);
+    std::unique_ptr<uint8_t[]> dest;
+    isCompressed = apperImpl.UnzipData(dest, dataLen);
+    EXPECT_FALSE(isCompressed);
+    apperImpl.extractor_.reset();
+    isCompressed = apperImpl.UnzipData(dest, dataLen);
+    EXPECT_FALSE(isCompressed);
 }
 }
 } // namespace NWeb
