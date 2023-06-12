cdc7dfae218371116a48939461ec5ab476467c69
zgit2021 <zhaowenqiang14@huawei.com>
2023-06-12 19:58:43 +0800

    fix code warning
    Signed-off-by: zgit2021 <zhaowenqiang14@huawei.com>
    Change-Id: I7cab9ce7ed07331293a4d7ee19c24f88fc9df50b

--- interfaces/inner_api/src/web_clipboard_controller.cpp
+++ interfaces/inner_api/src/web_clipboard_controller.cpp
@@ -23,11 +23,11 @@ const std::string IMG_TAG_SRC_PATTERN = "src='(.*?)'";
 const std::string IMG_TAG_SRC_HEAD = "src=\"";
 const std::string IMG_LOCAL_URI = "file:///";
 const std::string IMG_LOCAL_PATH = "://";
-constexpr int FOUR_BYTES = 4;
-constexpr int EIGHT_BIT = 8;
+constexpr uint32_t FOUR_BYTES = 4;
+constexpr uint32_t EIGHT_BIT = 8;
 
 struct Cmp {
-    bool operator()(const int& lhs, const int& rhs) const
+    bool operator()(const uint32_t& lhs, const uint32_t& rhs) const
     {
         return lhs > rhs;
     }
@@ -46,7 +46,7 @@ WebClipboardController& WebClipboardController::GetInstance()
 
 std::shared_ptr<MiscServices::PasteData> WebClipboardController::SplitHtml(std::shared_ptr<std::string> html) noexcept
 {
-    std::vector<std::pair<std::string, int>> matchVec = SplitHtmlWithImgLabel(html);
+    std::vector<std::pair<std::string, uint32_t>> matchVec = SplitHtmlWithImgLabel(html);
     std::map<std::string, std::vector<uint8_t>> imgSrcMap = SplitHtmlWithImgSrcLabel(matchVec);
     std::shared_ptr<MiscServices::PasteData> pasteData = BuildPasteData(html, imgSrcMap);
     return pasteData;
@@ -57,7 +57,7 @@ std::shared_ptr<std::string> WebClipboardController::RebuildHtml(
 {
     std::vector<std::shared_ptr<MiscServices::PasteDataRecord>> pasteDataRecords = pasteData->AllRecords();
     std::shared_ptr<std::string> htmlData;
-    std::map<int, std::pair<std::string, std::string>, Cmp> replaceUris;
+    std::map<uint32_t, std::pair<std::string, std::string>, Cmp> replaceUris;
 
     for (auto& item : pasteDataRecords) {
         std::shared_ptr<std::string> html = item->GetHtmlText();
@@ -72,9 +72,10 @@ std::shared_ptr<std::string> WebClipboardController::RebuildHtml(
         std::map<std::string, std::vector<uint8_t>> customItemData = customData->GetItemData();
         for (auto& itemData : customItemData) {
             for (auto i = 0; i < itemData.second.size(); i += FOUR_BYTES) {
-                int offset = static_cast<int>(itemData.second[i]) | static_cast<int>(itemData.second[i + 1] << 8) |
-                             static_cast<int>(itemData.second[i + 2] << 16) |
-                             static_cast<int>(itemData.second[i + 3] << 24);
+                uint32_t offset = static_cast<uint32_t>(itemData.second[i]) |
+                                  static_cast<uint32_t>(itemData.second[i + 1] << 8) |
+                                  static_cast<uint32_t>(itemData.second[i + 2] << 16) |
+                                  static_cast<uint32_t>(itemData.second[i + 3] << 24);
                 replaceUris[offset] = std::make_pair(uri->ToString(), itemData.first);
             }
         }
@@ -88,7 +89,7 @@ std::shared_ptr<std::string> WebClipboardController::RebuildHtml(
     return htmlData;
 }
 
-std::vector<std::pair<std::string, int>> WebClipboardController::SplitHtmlWithImgLabel(
+std::vector<std::pair<std::string, uint32_t>> WebClipboardController::SplitHtmlWithImgLabel(
     const std::shared_ptr<std::string> html) noexcept
 {
     std::smatch results;
@@ -96,12 +97,12 @@ std::vector<std::pair<std::string, int>> WebClipboardController::SplitHtmlWithIm
     std::regex r(pattern);
     std::string::const_iterator iterStart = html->begin();
     std::string::const_iterator iterEnd = html->end();
-    std::vector<std::pair<std::string, int>> matchVec;
+    std::vector<std::pair<std::string, uint32_t>> matchVec;
 
     while (std::regex_search(iterStart, iterEnd, results, r)) {
         std::string tmp = results[0];
         iterStart = results[0].second;
-        int offset = static_cast<int>(iterStart - html->begin()) - tmp.size();
+        uint32_t offset = static_cast<uint32_t>(results[0].first - html->begin());
 
         matchVec.emplace_back(std::make_pair(tmp, offset));
     }
@@ -110,7 +111,7 @@ std::vector<std::pair<std::string, int>> WebClipboardController::SplitHtmlWithIm
 }
 
 std::map<std::string, std::vector<uint8_t>> WebClipboardController::SplitHtmlWithImgSrcLabel(
-    const std::vector<std::pair<std::string, int>>& matchVec) noexcept
+    const std::vector<std::pair<std::string, uint32_t>>& matchVec) noexcept
 {
     std::map<std::string, std::vector<uint8_t>> res;
     std::smatch match;
@@ -122,14 +123,14 @@ std::map<std::string, std::vector<uint8_t>> WebClipboardController::SplitHtmlWit
         while (std::regex_search(iterStart, iterEnd, match, re)) {
             std::string tmp = match[0];
             iterStart = match[0].second;
-            int offset = static_cast<int>(iterStart - node.first.begin()) - tmp.size();
+            uint32_t offset = static_cast<uint32_t>(match[0].first - node.first.begin());
             tmp = tmp.substr(IMG_TAG_SRC_HEAD.size());
             tmp.pop_back();
             if (!IsLocalURI(tmp)) {
                 continue;
             }
             offset += IMG_TAG_SRC_HEAD.size() + node.second;
-            for (int i = 0; i < FOUR_BYTES; i++) {
+            for (uint32_t i = 0; i < FOUR_BYTES; i++) {
                 res[tmp].emplace_back((offset >> (EIGHT_BIT * i)) & 0xff);
             }
         }
