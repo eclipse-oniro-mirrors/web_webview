3ba05aa10f1791fd61218642f1d0a9e9caca18f4
yangguangzhao <yangguangzhao1@huawei.com>
2024-02-28 10:52:40 +0800

    add native arkweb api
    Signed-off-by: yangguangzhao <yangguangzhao1@huawei.com>

--- interfaces/kits/napi/webviewcontroller/webview_controller.h
+++ interfaces/kits/napi/webviewcontroller/webview_controller.h
@@ -91,7 +91,7 @@ class WebviewController {
 public:
     explicit WebviewController() = default;
     explicit WebviewController(int32_t nwebId);
-    explicit WebviewController(const std::string& webTag) : webTag_(webTag) {};
+    explicit WebviewController(const std::string& webTag);
     ~WebviewController();
 
     bool IsInit();
