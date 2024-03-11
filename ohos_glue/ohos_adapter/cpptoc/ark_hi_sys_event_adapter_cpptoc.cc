e70f7bb7673685ef90a259b6d7125623f7c64dbd
juwenjing <juwenjing@huawei.com>
2024-03-11 15:20:28 +0800

    code
    Signed-off-by: juwenjing <juwenjing@huawei.com>

--- ohos_glue/ohos_adapter/cpptoc/ark_hi_sys_event_adapter_cpptoc.cc
+++ ohos_glue/ohos_adapter/cpptoc/ark_hi_sys_event_adapter_cpptoc.cc
@@ -208,6 +208,77 @@ int ARK_WEB_CALLBACK ark_hi_sys_event_adapter_write8(struct _ark_hi_sys_event_ad
       key6,
       value6);
 }
+
+int ARK_WEB_CALLBACK ark_hi_sys_event_adapter_write9(struct _ark_hi_sys_event_adapter_t* self,
+  const ArkWebString* eventName, uint32_t type, const ArkWebString key1, const int64_t value1,
+  const ArkWebString key2, const int64_t value2, const ArkWebString key3, const uint32_t value3,
+  const ArkWebString key4, const int64_t value4, const ArkWebString key5, const int64_t value5,
+  const ArkWebString key6, const int64_t value6, const ArkWebString key7, const int64_t value7,
+  const ArkWebString key8, const int64_t value8, const ArkWebString key9, const int64_t value9,
+  const ArkWebString key10, const int64_t value10, const ArkWebString key11, const int64_t value11,
+  const ArkWebString key12, const int64_t value12, const ArkWebString key13, const int64_t value13,
+  const ArkWebString key14, const int64_t value14, const ArkWebString key15, const int64_t value15,
+  const ArkWebString key16, const int64_t value16, const ArkWebString key17, const int64_t value17,
+  const ArkWebString key18, const int64_t value18, const ArkWebString key19, const int64_t value19,
+  const ArkWebString key20, const int64_t value20, const ArkWebString key21, const int64_t value21,
+  const ArkWebString key22, const int64_t value22, const ArkWebString key23, const int64_t value23) {
+  ARK_WEB_CPPTOC_DV_LOG("capi struct is %{public}ld", (long)self);
+
+  ARK_WEB_CPPTOC_CHECK_PARAM(self,  0);
+
+  ARK_WEB_CPPTOC_CHECK_PARAM(eventName,  0);
+
+  // Execute
+  return ArkHiSysEventAdapterCppToC::Get(self)->Write(
+      *eventName,
+      type,
+      key1,
+      value1,
+      key2,
+      value2,
+      key3,
+      value3,
+      key4,
+      value4,
+      key5,
+      value5,
+      key6,
+      value6,
+      key7,
+      value7,
+      key8,
+      value8,
+      key9,
+      value9,
+      key10,
+      value10,
+      key11,
+      value11,
+      key12,
+      value12,
+      key13,
+      value13,
+      key14,
+      value14,
+      key15,
+      value15,
+      key16,
+      value16,
+      key17,
+      value17,
+      key18,
+      value18,
+      key19,
+      value19,
+      key20,
+      value20,
+      key21,
+      value21,
+      key22,
+      value22,
+      key23,
+      value23);
+}
 }  // namespace
 
 
@@ -220,6 +291,7 @@ ArkHiSysEventAdapterCppToC::ArkHiSysEventAdapterCppToC() {
   GetStruct()->write6 = ark_hi_sys_event_adapter_write6;
   GetStruct()->write7 = ark_hi_sys_event_adapter_write7;
   GetStruct()->write8 = ark_hi_sys_event_adapter_write8;
+  GetStruct()->write9 = ark_hi_sys_event_adapter_write9;
 }
 
 ArkHiSysEventAdapterCppToC::~ArkHiSysEventAdapterCppToC() {}
