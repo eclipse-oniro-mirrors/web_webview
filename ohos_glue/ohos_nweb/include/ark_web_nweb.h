06c86dc8687635577863d947470a074f5501874d
zgit2021 <zhaowenqiang14@huawei.com>
2024-03-23 22:49:02 +0800

    nweb add ScrollByRefScreen interface
    Signed-off-by: zgit2021 <zhaowenqiang14@huawei.com>
    Change-Id: I5281f0b571fa2a374b1d0c7d5f4e76e8c8b392db

--- ohos_glue/ohos_nweb/include/ark_web_nweb.h
+++ ohos_glue/ohos_nweb/include/ark_web_nweb.h
@@ -1137,6 +1137,17 @@ public:
   virtual ArkWebString GetLastJavascriptProxyCallingFrameUrl() = 0;
 
   /**
+   * @brief Scroll by the delta distance or velocity takes the screen as a reference.
+   *
+   * @param delta_x horizontal offset in physical pixel.
+   * @param delta_y vertical offset in physical pixel.
+   * @param vx      horizontal velocity in physical pixel.
+   * @param vx      vertical velocity in physical pixel.
+   */
+  /*--ark web()--*/
+  virtual void ScrollByRefScreen(float delta_x, float delta_y, float vx, float vy) = 0;
+
+  /**
    * Get pendingsize status.
    * @return result of last pendingsize status.
    */
