ed9c0a0ff2d130acc9f35f9933024f2d5aa6f883
lichenwei <lichenwei2@huawei.com>
2024-04-06 11:26:10 +0800

    使用dlopen调用AI子系统能力
    Signed-off-by: lichenwei <lichenwei2@huawei.com>

--- ohos_adapter/ai_engine_adapter/include/ai_engine_adapter_impl.h
+++ ohos_adapter/ai_engine_adapter/include/ai_engine_adapter_impl.h
@@ -16,9 +16,31 @@
 #ifndef AI_ENGINE_ADAPTER_IMPL_H
 #define AI_ENGINE_ADAPTER_IMPL_H
 
+#include <dlfcn.h>
+
 #include "ai_engine_adapter.h"
 
 namespace OHOS::NWeb {
+namespace {
+#ifdef __aarch64__
+constexpr char AI_ADAPTER_SO_PATH[] = "system/lib64/libai_text_analyzer_innerapi.z.so";
+#else
+constexpr char AI_ADAPTER_SO_PATH[] = "system/lib/libai_text_analyzer_innerapi.z.so";
+#endif
+} // namespace
+
+class DataDetectorInterface {
+public:
+    virtual std::vector<int8_t> GetWordSelection(const std::string& text, int8_t offset)
+    {
+        return std::vector<int8_t> { -1, -1 };
+    }
+
+protected:
+    virtual ~DataDetectorInterface() {}
+};
+using DataDetectorInstance = std::unique_ptr<DataDetectorInterface, std::function<void (DataDetectorInterface*)>>;
+
 class AiEngineAdapterImpl : public AiEngineAdapter {
 public:
     static AiEngineAdapterImpl& GetInstance();
@@ -33,6 +55,12 @@ private:
     AiEngineAdapterImpl(const AiEngineAdapterImpl&) = delete;
 
     AiEngineAdapterImpl& operator=(const AiEngineAdapterImpl&) = delete;
+
+    void *mLibraryHandle_ = dlopen(AI_ADAPTER_SO_PATH, RTLD_LAZY);
+    OHOS::NWeb::DataDetectorInterface* (*mCreateDataDetectorInstance_)() = (OHOS::NWeb::DataDetectorInterface* (*)())dlsym(mLibraryHandle_,
+        "OHOS_NWeb_createDataDetectorInstance");
+    void (*mDestoryDataDetectorInstance_)(OHOS::NWeb::DataDetectorInterface*) = (void (*)(OHOS::NWeb::DataDetectorInterface*))dlsym(
+        mLibraryHandle_, "OHOS_NWeb_destroyDataDetectorInstance");
 };
 } // namespace OHOS::NWeb
 
