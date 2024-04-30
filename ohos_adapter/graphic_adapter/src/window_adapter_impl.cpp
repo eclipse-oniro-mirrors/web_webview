039b8d643f8f6df46807903be6fb9b1fd22fa00f
xzc <xuzhechu@huawei.com>
2024-04-30 08:28:14 +0000

    ion optimization: check pointers
    Signed-off-by: xzc <xuzhechu@huawei.com>
    Change-Id: I033d338dfe001df5c2a8cee5fbf507a0a6edd816

--- ohos_adapter/graphic_adapter/src/window_adapter_impl.cpp
+++ ohos_adapter/graphic_adapter/src/window_adapter_impl.cpp
@@ -53,6 +53,11 @@ int32_t WindowAdapterImpl::NativeWindowSetBufferGeometry(NWebNativeWindow window
 void WindowAdapterImpl::NativeWindowSurfaceCleanCache(NWebNativeWindow window)
 {
     WVLOG_D("WindowAdapterImpl::NativeWindowSurfaceCleanCache");
-    reinterpret_cast<OHNativeWindow*>(window)->surface->CleanCache();
+    auto nativeWindow = reinterpret_cast<OHNativeWindow*>(window);
+    if (!nativeWindow || !nativeWindow->surface) {
+        WVLOG_D("window or surface is null, no need to clean surface cache");
+        return;
+    }
+    nativeWindow->surface->CleanCache();
 }
 } // namespace OHOS::NWeb
