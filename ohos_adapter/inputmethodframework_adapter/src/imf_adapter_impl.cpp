531f962d04d78742b697b515173e14def927a73e
zourongchun <zourongchun@huawei.com>
2024-05-16 16:06:23 +0800

    add preview support
    Signed-off-by: zourongchun <zourongchun@huawei.com>

--- ohos_adapter/inputmethodframework_adapter/src/imf_adapter_impl.cpp
+++ ohos_adapter/inputmethodframework_adapter/src/imf_adapter_impl.cpp
@@ -283,7 +283,7 @@ bool IMFAdapterImpl::Attach(std::shared_ptr<IMFTextListenerAdapter> listener, bo
 
     MiscServices::InputAttribute inputAttribute = { .inputPattern = config->GetInputAttribute()->GetInputPattern(),
         .enterKeyType = config->GetInputAttribute()->GetEnterKeyType(),
-        .isTextPreviewSupported = false };
+        .isTextPreviewSupported = true };
 
     MiscServices::CursorInfo imfInfo = { .left = config->GetCursorInfo()->GetLeft(),
         .top = config->GetCursorInfo()->GetTop(),
