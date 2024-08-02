b6f1207d501c6f868ec000e70d2a9c31bc6ffd8f
zouzhiwei <zouzhiwei2@huawei.com>
2024-08-02 16:36:07 +0800

    web_add_fuzz2
    Signed-off-by: zouzhiwei <zouzhiwei2@huawei.com>

---
+++ test/fuzztest/ohos_adapter/media_adapter/codecformatadapterimpl_fuzzer/codecformatadapterimpl_fuzzer.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "codecformatadapterimpl_fuzzer.h"
+
+#include <securec.h>
+
+#include "codec_format_adapter_impl.h"
+
+using namespace OHOS::NWeb;
+
+namespace OHOS {
+bool CreateLocationProxyAdapterFuzzTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size == 0)) {
+        return false;
+    }
+    OHOS::NWeb::CodecFormatAdapterImpl codecFormatAdapter;
+    int32_t width;
+    int32_t height;
+    if (memcpy_s(&width, sizeof(int32_t), data, sizeof(int32_t)) != 0) {
+        return true;
+    }
+    if (memcpy_s(&height, sizeof(int32_t), data, sizeof(int32_t)) != 0) {
+        return true;
+    }
+    codecFormatAdapter.SetWidth(width);
+    codecFormatAdapter.SetHeight(height);
+
+    codecFormatAdapter.GetWidth();
+    codecFormatAdapter.GetHeight();
+    return true;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::CreateLocationProxyAdapterFuzzTest(data, size);
+    return 0;
+}
