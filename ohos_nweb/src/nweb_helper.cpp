4e45bb1b1b4e5a8d523cd6120be07d489627dcde
廖云帆 <liaoyunfan@huawei.com>
2024-08-13 14:57:44 +0800

    补全错误日志
    Signed-off-by: 廖云帆 <liaoyunfan@huawei.com>

--- ohos_nweb/src/nweb_helper.cpp
+++ ohos_nweb/src/nweb_helper.cpp
@@ -608,6 +608,7 @@ bool NWebHelper::LoadLib(bool from_ark)
         return true;
     }
     if (bundlePath_.empty()) {
+        WVLOG_E("load lib failed, because bundle path is empty");
         return false;
     }
     std::string loadLibPath;
@@ -678,7 +679,7 @@ void* NWebHelper::GetWebEngineHandler()
     const std::string& bundle_path = ctx->GetBundleCodeDir();
     SetBundlePath(bundle_path);
     if (!Init(true)) {
-        WVLOG_I("NWebHelper Init failed");
+        WVLOG_E("NWebHelper Init failed");
         return nullptr;
     }
     return libHandleWebEngine_;
@@ -713,14 +714,14 @@ static void DoPreReadLib(const std::string &bundlePath)
 
     char tempPath[PATH_MAX] = {0};
     if (realpath(libPathWebEngine.c_str(), tempPath) == nullptr) {
-        WVLOG_E("path to realpath error");
+        WVLOG_E("path to realpath error, errno(%{public}d):%{public}s", errno, strerror(errno));
         return;
     }
 
     struct stat stats;
     int ret = stat(tempPath, &stats);
     if (ret < 0) {
-        WVLOG_E("stat web engine library failed, ret = %{public}d", ret);
+        WVLOG_E("stat web engine library failed, errno(%{public}d):%{public}s", errno, strerror(errno));
         return;
     }
 
@@ -733,7 +734,7 @@ static void DoPreReadLib(const std::string &bundlePath)
 
     int fd = open(tempPath, O_RDONLY);
     if (fd <= 0) {
-        WVLOG_E("open web engine library failed");
+        WVLOG_E("open web engine library failed, errno(%{public}d):%{public}s", errno, strerror(errno));
         delete[] buf;
         return;
     }
