c7a4a7887977896256052b4bd4f21090bae18558
LXinJie <liuxinjie6@huawei.com>
2024-09-05 20:39:56 +0800

    liuxinjie6@huawei.com
    Signed-off-by: LXinJie <liuxinjie6@huawei.com>

--- ohos_adapter/aafwk_adapter/src/aafwk_browser_client_adapter_impl.cpp
+++ ohos_adapter/aafwk_adapter/src/aafwk_browser_client_adapter_impl.cpp
@@ -156,6 +156,10 @@ void* AafwkBrowserClientAdapterImpl::QueryRenderSurface(int32_t surface_id)
         sptr<IBufferProducer> bufferProducer = iface_cast<IBufferProducer>(surfaceObject);
         sptr<Surface> surface = Surface::CreateSurfaceAsProducer(bufferProducer);
         OHNativeWindow* window = ::CreateNativeWindowFromSurface(&surface);
+        if (!window) {
+            WVLOG_E("create the native window from surface failed.");
+            return nullptr;
+        }
         window_map_.emplace(surface_id, window);
         void* newNativeWindow = reinterpret_cast<NWebNativeWindow>(window);
         WVLOG_D("receive surface num = %{public}d", surface_id);
@@ -210,6 +214,11 @@ void AafwkBrowserClientAdapterImpl::DestroyRenderSurface(int32_t surface_id)
     if (window_map_.find(surface_id) != window_map_.end()) {
         OHNativeWindow* window = window_map_[surface_id];
         ::DestoryNativeWindow(window);
+        if (!window) {
+            WVLOG_D("window has been destroyed successfully.");
+        } else {
+            WVLOG_E("failed to destroy the window, there may be some leak.");
+        }
         window_map_.erase(surface_id);
     }
     GetInstance().browserHost_->DestroyRenderSurface(surface_id);
