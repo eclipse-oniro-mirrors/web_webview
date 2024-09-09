14fffb1328b1633af8a24118bcd7869e9e2469e7
chenzhiz <chenzhizhang1@huawei.com>
2024-09-09 16:24:06 +0800

    编程告警规范修改
    Signed-off-by: chenzhiz <chenzhizhang1@huawei.com>

--- interfaces/kits/napi/web_net_error_code/napi_web_net_errorcode.cpp
+++ interfaces/kits/napi/web_net_error_code/napi_web_net_errorcode.cpp
@@ -22,9 +22,6 @@ napi_value ToInt32Value(napi_env env, int32_t number)
 {
     napi_value result = nullptr;
     napi_status status = napi_create_int32(env, number, &result);
-    if (status != napi_status::napi_ok){
-        return result;
-    }
     return result;
 }
 
