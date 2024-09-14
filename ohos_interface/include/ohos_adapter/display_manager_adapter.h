3254b1925df80f223106323561e7d149da3bb3a7
ia_oi <jiangjiacheng3@h-partners.com>
2024-09-14 17:43:53 +0800

    回退
    Signed-off-by: ia_oi <jiangjiacheng3@h-partners.com>

--- ohos_interface/include/ohos_adapter/display_manager_adapter.h
+++ ohos_interface/include/ohos_adapter/display_manager_adapter.h
@@ -51,15 +51,6 @@ enum class DisplayOrientation : uint32_t {
     UNKNOWN,
 };
 
-enum class FoldStatus : uint32_t {
-    UNKNOWN = 0,
-    FULL = 1,
-    MAIN = 2,
-    SUB = 3,
-    COORDINATION = 4,
-};
-
-
 class DisplayListenerAdapter {
 public:
     DisplayListenerAdapter() = default;
@@ -71,15 +62,6 @@ public:
     virtual void OnDestroy(DisplayId) = 0;
 
     virtual void OnChange(DisplayId) = 0;
-};
-
-class FoldStatusListenerAdapter {
-public:
-    FoldStatusListenerAdapter() = default;
-
-    virtual ~FoldStatusListenerAdapter() = default;
-
-    virtual void OnFoldStatusChanged(FoldStatus foldstatus) {};
 };
 
 class DisplayAdapter {
@@ -103,14 +85,6 @@ public:
     virtual int32_t GetDpi() = 0;
 
     virtual DisplayOrientation GetDisplayOrientation() = 0;
-
-    virtual FoldStatus GetFoldStatus() {
-        return FoldStatus::UNKNOWN;
-    };
-
-    virtual bool IsFoldable(){
-        return false;
-    };
 };
 
 class DisplayManagerAdapter {
@@ -128,14 +102,7 @@ public:
     virtual bool UnregisterDisplayListener(ListenerId id) = 0;
 
     virtual bool IsDefaultPortrait() = 0;
-    
-    virtual uint32_t RegisterFoldStatusListener(std::shared_ptr<FoldStatusListenerAdapter> listener) {
-        return -1;
-    };
 
-    virtual bool UnregisterFoldStatusListener(uint32_t id) {
-        return false;
-    };
 };
 
 } // namespace OHOS::NWeb
