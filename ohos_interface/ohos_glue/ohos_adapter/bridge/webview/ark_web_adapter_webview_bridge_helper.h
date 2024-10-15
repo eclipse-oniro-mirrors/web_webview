478d431a654690555a8a3a284497fb917b5a9df9
zhangyanchuan <zhangyanchuan@huawei.com>
2024-10-15 22:49:10 +0800

    gule层解除接口顺序依赖
    Signed-off-by: zhangyanchuan <zhangyanchuan@huawei.com>

--- ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_web_adapter_webview_bridge_helper.h
+++ ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_web_adapter_webview_bridge_helper.h
@@ -13,16 +13,26 @@
  * limitations under the License.
  */
 
-#ifndef NWEB_API_LEVEL_H_
-#define NWEB_API_LEVEL_H_
+#ifndef ARK_WEB_ADAPTER_WEBVIEW_BRIDGE_HELPER_H_
+#define ARK_WEB_ADAPTER_WEBVIEW_BRIDGE_HELPER_H_
+#pragma once
 
-/*
- * API level consists of API version and ArkWeb monthly version. The last three digits
- * are ArkWeb monthly version. If the ArkWeb minor version is less than three digits, it
- * is padded with 0. For example, if the API version is 12 and the ArkWeb monthly version
- * is 1, then API level is 12001.
- */
-#define ARKWEB_CORE_API_LEVEL 14001
+#include "base/bridge/ark_web_bridge_helper.h"
 
-#endif  // NWEB_API_LEVEL_H_
+namespace OHOS::ArkWeb {
 
+class ArkWebAdapterWebviewBridgeHelper : public ArkWebBridgeHelper {
+public:
+    ~ArkWebAdapterWebviewBridgeHelper() = default;
+
+    static ArkWebAdapterWebviewBridgeHelper& GetInstance();
+
+private:
+    ArkWebAdapterWebviewBridgeHelper();
+
+    void Init();
+};
+
+} // namespace OHOS::ArkWeb
+
+#endif // ARK_WEB_ADAPTER_WEBVIEW_BRIDGE_HELPER_H_
