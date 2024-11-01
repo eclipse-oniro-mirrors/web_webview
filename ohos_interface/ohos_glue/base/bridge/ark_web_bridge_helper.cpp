4edf4bfbae3070f4eee9da30385501a520db4da5
zhaowenpu <zhaowenpu1@huawei.com>
2024-11-01 08:24:30 +0000

    update ohos_interface/ohos_glue/base/bridge/ark_web_bridge_helper.cpp.
    Signed-off-by: zhaowenpu <zhaowenpu1@huawei.com>

--- ohos_interface/ohos_glue/base/bridge/ark_web_bridge_helper.cpp
+++ ohos_interface/ohos_glue/base/bridge/ark_web_bridge_helper.cpp
@@ -15,7 +15,6 @@
 
 #include "base/bridge/ark_web_bridge_helper.h"
 
-#include <dlfcn.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -67,7 +66,7 @@ bool ArkWebBridgeHelper::LoadLibFile(int openMode, const std::string& libNsName,
     dlns_create(&dlns, libDirPath.c_str());
 
     Dl_namespace ndkns;
-    dln_get("ndkns", &ndkns);
+    dlns_get("ndkns", &ndkns);
     dlns_inherit(&dlns, &ndkns, "allow_all_shared_libs");
 
     void* libFileHandler = dlopen_ns(&dlns, libFileName.c_str(), openMode);
