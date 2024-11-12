7865b63bd01bf31a4f5d3049333276085cc1c152
wuqiwen <wuqiwen7@huawei.com>
2024-11-12 13:49:36 +0000

    update ohos_adapter/media_adapter/src/player_framework_adapter_impl.cpp.
    Signed-off-by: wuqiwen <wuqiwen7@huawei.com>

--- ohos_adapter/media_adapter/src/player_framework_adapter_impl.cpp
+++ ohos_adapter/media_adapter/src/player_framework_adapter_impl.cpp
@@ -40,7 +40,6 @@ bool IsUnsupportType(int32_t errorCode)
         case Media::MSERR_UNSUPPORT_STREAM:
         case Media::MSERR_UNSUPPORT_FILE:
         case Media::MSERR_UNSUPPORT_SOURCE:
-        case Media::MSERR_DATA_SOURCE_IO_ERROR:
             return true;
         default:
             return false;
@@ -57,6 +56,7 @@ bool IsFatalError(int32_t errorCode)
         case Media::MSERR_AUD_DEC_FAILED:
         case Media::MSERR_VID_DEC_FAILED:
         case Media::MSERR_FILE_ACCESS_FAILED:
+        case Media::MSERR_DATA_SOURCE_IO_ERROR:
             return true;
         default:
             return false;
