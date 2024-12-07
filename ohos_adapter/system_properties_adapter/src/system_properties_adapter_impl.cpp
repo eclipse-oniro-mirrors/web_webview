8bf651943dfb961f81006b33816c0c18b96e7086
taoxueao <taoxueao1@huawei.com>
2024-12-07 01:17:02 +0000

    update ohos_adapter/system_properties_adapter/src/system_properties_adapter_impl.cpp.
    Signed-off-by: taoxueao <taoxueao1@huawei.com>

--- ohos_adapter/system_properties_adapter/src/system_properties_adapter_impl.cpp
+++ ohos_adapter/system_properties_adapter/src/system_properties_adapter_impl.cpp
@@ -173,11 +173,6 @@ std::string SystemPropertiesAdapterImpl::GetUserAgentOSName()
     return OHOS::system::GetParameter("const.product.os.dist.name", "");
 }
 
-std::string SystemPropertiesAdapterImpl::GetDeviceInfoApiVersion()
-{
-    return OHOS::system::GetParameter("const.product.os.dist.apiversion", "");
-}
-
 std::string SystemPropertiesAdapterImpl::GetUserAgentOSVersion()
 {
     return OHOS::system::GetParameter("const.product.os.dist.apiname", "").empty() ?
@@ -374,5 +369,10 @@ std::string SystemPropertiesAdapterImpl::GetCompatibleDeviceType()
     free(compatibleDeviceType);
     
     return deviceType;
+}
+
+std::string SystemPropertiesAdapterImpl::GetDeviceInfoApiVersion()
+{
+    return OHOS::system::GetParameter("const.product.os.dist.apiversion", "");
 }
 } // namespace OHOS::NWeb
