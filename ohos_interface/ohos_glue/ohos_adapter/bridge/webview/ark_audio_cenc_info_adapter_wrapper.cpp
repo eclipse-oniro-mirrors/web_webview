23554a76e44a0b363417997a44a2a3eea1fded87
y00699229 <yangjiawei24@huawei.com>
2024-12-30 23:24:14 +0800

    add audio decoder
    Signed-off-by: y00699229 <yangjiawei24@huawei.com>

---
+++ ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_audio_cenc_info_adapter_wrapper.cpp
@@ -0,0 +1,150 @@
+/*
+  * Copyright (c) 2024 Huawei Device Co., Ltd.
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+#include "ohos_adapter/bridge/ark_audio_cenc_info_adapter_wrapper.h"
+#include "base/bridge/ark_web_bridge_macros.h"
+
+namespace OHOS::ArkWeb {
+
+ArkAudioCencInfoAdapterWrapper::ArkAudioCencInfoAdapterWrapper(ArkWebRefPtr<ArkAudioCencInfoAdapter> ref) : ctocpp_(ref)
+{}
+
+uint8_t* ArkAudioCencInfoAdapterWrapper::GetKeyId()
+{
+    return ctocpp_->GetKeyId();
+}
+
+uint32_t ArkAudioCencInfoAdapterWrapper::GetKeyIdLen()
+{
+    return ctocpp_->GetKeyIdLen();
+}
+
+uint8_t* ArkAudioCencInfoAdapterWrapper::GetIv()
+{
+    return ctocpp_->GetIv();
+}
+
+uint32_t ArkAudioCencInfoAdapterWrapper::GetIvLen()
+{
+    return ctocpp_->GetIvLen();
+}
+
+uint32_t ArkAudioCencInfoAdapterWrapper::GetAlgo()
+{
+    return ctocpp_->GetAlgo();
+}
+
+uint32_t ArkAudioCencInfoAdapterWrapper::GetEncryptedBlockCount()
+{
+    return ctocpp_->GetEncryptedBlockCount();
+}
+
+uint32_t ArkAudioCencInfoAdapterWrapper::GetSkippedBlockCount()
+{
+    return ctocpp_->GetSkippedBlockCount();
+}
+
+uint32_t ArkAudioCencInfoAdapterWrapper::GetFirstEncryptedOffset()
+{
+    return ctocpp_->GetFirstEncryptedOffset();
+}
+
+std::vector<uint32_t> ArkAudioCencInfoAdapterWrapper::GetClearHeaderLens()
+{
+    ArkWebUint32Vector lens = ctocpp_->GetClearHeaderLens();
+
+    std::vector<uint32_t> objLens = ArkWebBasicVectorStructToClass<uint32_t, ArkWebUint32Vector>(lens);
+    ArkWebBasicVectorStructRelease<ArkWebUint32Vector>(lens);
+    return objLens;
+}
+
+std::vector<uint32_t> ArkAudioCencInfoAdapterWrapper::GetPayLoadLens()
+{
+    ArkWebUint32Vector lens = ctocpp_->GetPayLoadLens();
+
+    std::vector<uint32_t> objLens = ArkWebBasicVectorStructToClass<uint32_t, ArkWebUint32Vector>(lens);
+    ArkWebBasicVectorStructRelease<ArkWebUint32Vector>(lens);
+    return objLens;
+}
+
+uint32_t ArkAudioCencInfoAdapterWrapper::GetMode()
+{
+    return ctocpp_->GetMode();
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetKeyId(uint8_t* keyId)
+{
+    ctocpp_->SetKeyId(keyId);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetKeyIdLen(uint32_t keyIdLen)
+{
+    ctocpp_->SetKeyIdLen(keyIdLen);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetIv(uint8_t* iv)
+{
+    ctocpp_->SetIv(iv);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetIvLen(uint32_t ivLen)
+{
+    ctocpp_->SetIvLen(ivLen);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetAlgo(uint32_t algo)
+{
+    ctocpp_->SetAlgo(algo);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetEncryptedBlockCount(uint32_t count)
+{
+    ctocpp_->SetEncryptedBlockCount(count);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetSkippedBlockCount(uint32_t count)
+{
+    ctocpp_->SetSkippedBlockCount(count);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetFirstEncryptedOffset(uint32_t offset)
+{
+    ctocpp_->SetFirstEncryptedOffset(offset);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetClearHeaderLens(const std::vector<uint32_t>& clearHeaderLens)
+{
+    ArkWebUint32Vector lens = ArkWebBasicVectorClassToStruct<uint32_t, ArkWebUint32Vector>(clearHeaderLens);
+
+    ctocpp_->SetClearHeaderLens(lens);
+
+    ArkWebBasicVectorStructRelease<ArkWebUint32Vector>(lens);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetPayLoadLens(const std::vector<uint32_t>& payLoadLens)
+{
+    ArkWebUint32Vector lens = ArkWebBasicVectorClassToStruct<uint32_t, ArkWebUint32Vector>(payLoadLens);
+
+    ctocpp_->SetPayLoadLens(lens);
+
+    ArkWebBasicVectorStructRelease<ArkWebUint32Vector>(lens);
+}
+
+void ArkAudioCencInfoAdapterWrapper::SetMode(uint32_t mode)
+{
+    ctocpp_->SetMode(mode);
+}
+
+} // namespace OHOS::ArkWeb
---
+++ ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_audio_codec_decoder_adapter_impl.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARK_AUDIO_CODEC_DECODER_ADAPTER_IMPL_H
+#define ARK_AUDIO_CODEC_DECODER_ADAPTER_IMPL_H
+#pragma once
+
+#include "audio_codec_decoder_adapter.h"
+#include "ohos_adapter/include/ark_audio_codec_decoder_adapter.h"
+#include "audio_cenc_info_adapter.h"
+#include "ohos_adapter/include/ark_audio_cenc_info_adapter.h"
+
+namespace OHOS::ArkWeb {
+
+class ArkAudioCodecDecoderAdapterImpl : public ArkAudioCodecDecoderAdapter {
+public:
+    explicit ArkAudioCodecDecoderAdapterImpl(std::shared_ptr<OHOS::NWeb::AudioCodecDecoderAdapter>);
+
+    int32_t CreateAudioDecoderByMime(const ArkWebString& mimetype) override;
+
+    int32_t CreateAudioDecoderByName(const ArkWebString& name) override;
+
+    int32_t ConfigureDecoder(const ArkWebRefPtr<ArkAudioDecoderFormatAdapter> format) override;
+
+    int32_t SetParameterDecoder(const ArkWebRefPtr<ArkAudioDecoderFormatAdapter> format) override;
+
+    int32_t PrepareDecoder() override;
+
+    int32_t StartDecoder() override;
+
+    int32_t StopDecoder() override;
+
+    int32_t FlushDecoder() override;
+
+    int32_t ResetDecoder() override;
+
+    int32_t ReleaseDecoder() override;
+
+    int32_t QueueInputBufferDec(uint32_t index, int64_t presentationTimeUs, uint8_t *bufferData, int32_t bufferSize,
+        const ArkWebRefPtr<ArkAudioCencInfoAdapter> cencInfo, bool isEncrypted, uint32_t flag) override;
+
+    int32_t GetOutputFormatDec(ArkWebRefPtr<ArkAudioDecoderFormatAdapter> format) override;
+
+    int32_t ReleaseOutputBufferDec(uint32_t index) override;
+
+    int32_t SetCallbackDec(const ArkWebRefPtr<ArkAudioDecoderCallbackAdapter> callback) override;
+
+    int32_t SetDecryptionConfig(void *session, bool secureAudio) override;
+
+private:
+    std::shared_ptr<OHOS::NWeb::AudioCodecDecoderAdapter> real_;
+
+    IMPLEMENT_REFCOUNTING(ArkAudioCodecDecoderAdapterImpl);
+};
+
+} // namespace OHOS::ArkWeb
+
+#endif // ARK_AUDIO_CODEC_DECODER_ADAPTER_IMPL_H
