6ccef668727b4fcc6ce97498330a8cc15f50b07d
lilei <lilei433@h-partners.com>
2025-01-23 13:31:28 +0000

    update test/unittest/ohos_adapter/media_adapter_test/player_framework_adapter_impl_test.cpp.
    Signed-off-by: lilei <lilei433@h-partners.com>

--- test/unittest/ohos_adapter/media_adapter_test/player_framework_adapter_impl_test.cpp
+++ test/unittest/ohos_adapter/media_adapter_test/player_framework_adapter_impl_test.cpp
@@ -111,7 +111,7 @@ HWTEST_F(PlayerAdapterImplTest, NormalTest, TestSize.Level1)
     EXPECT_NE(playerAdapter_->GetDuration(duration_), -1);
     EXPECT_NE(playerAdapter_->SetPlaybackSpeed(PlaybackRateMode::SPEED_FORWARD_1_00_X), -1);
     std::map<std::string, std::string> header;
-    EXPECT_EQ(playerAdapter_->SetMediaSourceHeader(sourceUrl_, header), -1);
+    EXPECT_NE(playerAdapter_->SetMediaSourceHeader(sourceUrl_, header), -1);
 }
 
 /**
