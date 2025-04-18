6aea4e1543aeb4e2168fe51c959fa8ee77bc874a
TaoXueao <taoxueao1@huawei.com>
2025-04-18 16:10:34 +0800

    日志超限
    Signed-off-by: TaoXueao <taoxueao1@huawei.com>

--- ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_ohos_adapter_helper_impl.cpp
+++ ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_ohos_adapter_helper_impl.cpp
@@ -220,9 +220,13 @@ ArkWebRefPtr<ArkKeystoreAdapter> ArkOhosAdapterHelperImpl::GetKeystoreAdapterIns
 
 ArkWebRefPtr<ArkEnterpriseDeviceManagementAdapter> ArkOhosAdapterHelperImpl::GetEnterpriseDeviceManagementInstance()
 {
-    static NWeb::EnterpriseDeviceManagementAdapter& instance = real_.GetEnterpriseDeviceManagementInstance();
-    static ArkWebRefPtr<ArkEnterpriseDeviceManagementAdapter> impl =
-        new ArkEnterpriseDeviceManagementAdapterImpl(instance);
+    static std::once_flag flag;
+    static ArkWebRefPtr<ArkEnterpriseDeviceManagementAdapter> impl;
+    std::call_once(flag, [this] {
+        static NWeb::EnterpriseDeviceManagementAdapter& instance =
+            real_.GetEnterpriseDeviceManagementInstance();
+        impl = new ArkEnterpriseDeviceManagementAdapterImpl(instance);
+    });
     return impl;
 }
 
