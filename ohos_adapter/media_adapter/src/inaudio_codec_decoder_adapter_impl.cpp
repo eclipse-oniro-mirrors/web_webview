d60d6e7205852e148183b6efb7798c33f103ab47
雪地画猫 <yangjiawei24@huawei.com>
2025-05-08 07:11:06 +0000

    update ohos_adapter/media_adapter/src/inaudio_codec_decoder_adapter_impl.cpp.
    Signed-off-by: 雪地画猫 <yangjiawei24@huawei.com>

--- ohos_adapter/media_adapter/src/inaudio_codec_decoder_adapter_impl.cpp
+++ ohos_adapter/media_adapter/src/inaudio_codec_decoder_adapter_impl.cpp
@@ -21,7 +21,7 @@
 #include <multimedia/native_audio_channel_layout.h>
 #include "native_drm_err.h"
 #include "native_mediakeysession.h"
-#include <cinttypes>
+
 #include "audio_cenc_info_adapter_impl.h"
 
 #define MIME_SIZE 256
@@ -153,7 +153,7 @@ void AudioDecoderFormatAdapterImpl::PrintFormatData(std::shared_ptr<AudioDecoder
 {
     WVLOG_I("PrintFormatData:SampleRate[%{public}d], ChannelCount[%{public}d],"
             "MaxInputSize[%{public}d], AACIsAdts[%{public}d], AudioSampleFormat[%{public}d],"
-            "BitRate[%{public}" PRId64 "], IDHeader[%{public}d], SetupHeader[%{public}d],",
+            "BitRate[%{public}lld], IDHeader[%{public}d], SetupHeader[%{public}d],",
             format->GetSampleRate(), format->GetChannelCount(), format->GetMaxInputSize(),
             static_cast<int32_t>(format->GetAACIsAdts()), format->GetAudioSampleFormat(),
             format->GetBitRate(), format->GetIdentificationHeader(), format->GetSetupHeader());
