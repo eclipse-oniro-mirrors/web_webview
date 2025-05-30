d2ee21b28b9e9487191d2f5a8ea3043bd081d212
hoo334 <liminghu1@huawei.com>
2025-05-30 15:51:29 +0800

    新增AudioSessionType
    Signed-off-by: hoo334 <liminghu1@huawei.com>

--- ohos_interface/include/ohos_nweb/nweb.h
+++ ohos_interface/include/ohos_nweb/nweb.h
@@ -1852,6 +1852,13 @@ public:
                               int child_id,
                               int frame_routing_id,
                               int event) {}
+
+    /**
+     * Set audio session type.
+     *
+     * @param audioSessionType Audio session type.
+     */
+    virtual void SetAudioSessionType(int32_t audioSessionType) {}
 };
 
 } // namespace OHOS::NWeb
