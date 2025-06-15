5695aeefa643b3a16db2f6dd16aaf2ed24c7af64
duanshaofei <duanshaofei@huawei-partners.com>
2025-06-15 09:45:48 +0800

    magnifier_ux
    Signed-off-by: duanshaofei <duanshaofei@huawei-partners.com>

--- ohos_interface/ohos_glue/ohos_nweb/bridge/webcore/ark_web_nweb_impl.h
+++ ohos_interface/ohos_glue/ohos_nweb/bridge/webcore/ark_web_nweb_impl.h
@@ -1676,6 +1676,12 @@ public:
      */
     int32_t SetBlanklessLoadingWithKey(const ArkWebString& key, bool isStart) override;
 
+    /**
+     * @brief Update the single handle visible.
+     * @param isVisible The single handle visible.
+     */
+    void UpdateSingleHandleVisible(bool isVisible) override;
+
 private:
     std::shared_ptr<OHOS::NWeb::NWeb> nweb_nweb_;
 };
