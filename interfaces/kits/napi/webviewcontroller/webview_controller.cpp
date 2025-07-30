243e4f49611cc4c372f7bccb8cca4615757337d0
SprucEovo <lichenwei2@huawei.com>
2025-07-30 11:20:10 +0800

    交互动效适配双内核
    Signed-off-by: SprucEovo <lichenwei2@huawei.com>

--- interfaces/kits/napi/webviewcontroller/webview_controller.cpp
+++ interfaces/kits/napi/webviewcontroller/webview_controller.cpp
@@ -2157,7 +2157,9 @@ void WebviewController::GetScrollOffset(float* offset_x, float* offset_y)
 
 void WebviewController::GetPageOffset(float* offset_x, float* offset_y)
 {
-    RETURN_IF_CALLING_FROM_M114();
+    if (ArkWeb::getActiveWebEngineVersion() < ArkWeb::ArkWebEngineVersion::M132) {
+        return;
+    }
     auto nweb_ptr = NWebHelper::Instance().GetNWeb(nwebId_);
     if (nweb_ptr) {
         nweb_ptr->GetPageOffset(offset_x, offset_y);
