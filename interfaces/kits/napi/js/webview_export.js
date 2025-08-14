322f9b477675546dd17b64413f0b497ae18eed07
zhanghang133 <zhanghang133@h-partners.com>
2025-08-14 08:36:27 +0000

    update interfaces/kits/napi/js/webview_export.js.
    Signed-off-by: zhanghang133 <zhanghang133@h-partners.com>

--- interfaces/kits/napi/js/webview_export.js
+++ interfaces/kits/napi/js/webview_export.js
@@ -14,7 +14,7 @@
  */
 
 let cert = requireInternal('security.cert');
-let webview = requireNapi('web.webview');
+let webview = requireInternal('web.webview');
 let fileIo = requireNapi('file.fs');
 let fileUri = requireNapi('file.fileuri');
 let picker = requireNapi('file.picker');
